463c6b24ce74ed03d1f5d7190650de330ab4fccd
Diogo vieira <diogobgff@gmail.com>
2026-03-14 20:45:34 -0300

    feat(16-03): export CUAEpisodeRunner and CUAEpisodeResult from cua package

--- src/lunar_sandbox/cua/__init__.py
+++ src/lunar_sandbox/cua/__init__.py
@@ -2,6 +2,7 @@
 
 from lunar_sandbox.cua.observation import CUAObservation
 from lunar_sandbox.cua.retention import RetentionPolicy, cleanup_episodes
+from lunar_sandbox.cua.runner import CUAEpisodeResult, CUAEpisodeRunner
 from lunar_sandbox.cua.task import (
     CUATask,
     ManualReward,
@@ -19,4 +20,6 @@ __all__ = [
     "CUAObservation",
     "RetentionPolicy",
     "cleanup_episodes",
+    "CUAEpisodeRunner",
+    "CUAEpisodeResult",
 ]
